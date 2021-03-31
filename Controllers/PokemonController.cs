8afb65135ca77774a405d60ee4a60c5a8e4c72fa
John Z <johnziss9@gmail.com>
2021-03-31 16:49:43 +0100

    Removed the instance of HttpClient from the Get method and called it in the constructor.

--- Controllers/PokemonController.cs
+++ Controllers/PokemonController.cs
@@ -8,14 +8,19 @@ namespace Pokedex.Controllers
     [Route("[controller]")]
     public class PokemonController : ControllerBase
     {
+        private static HttpClient _httpClient;
+
+        public PokemonController()
+        {
+            _httpClient = new HttpClient();
+        }
+
         public async Task<string> Get(string name)
         {
             // Hard coded the name to test if url brings the correct data
             var url = $"https://pokeapi.co/api/v2/pokemon/pikachu";
             
-            // Create instance of HttpClient in order to call the PokeAPI
-            var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync(url);
+            var response = await _httpClient.GetAsync(url);
 
             return await response.Content.ReadAsStringAsync();
         }
